de930f79f07bc03916a9b819a8b8de8a92b114ab
Nipat Tuntasood <ntuntasood@gmail.com>
2023-12-01 01:11:26 -0800

    Add properties file

--- src/processors/Application.java
+++ src/processors/Application.java
@@ -3,6 +3,10 @@
  */
 package processors;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import processors.clients.ChatGPTClient;
 import processors.impl.TextOnlyProcessor;
 import processors.models.JobRequest;
@@ -13,9 +17,19 @@ import processors.models.JobResponse;
  */
 public class Application {
 	
+	private static Properties properties;
 	private static Processor textOnlyProcessor;
 	
 	public static void init() {
+				
+		properties = new Properties();
+		try {
+			properties.load(new FileInputStream("src/config.properties"));
+		} catch (IOException e) {
+			System.err.println("failed to open properties file");
+			e.printStackTrace();
+		}
+		
 		ChatGPTClient chatGPTClient = new ChatGPTClient();
 		textOnlyProcessor = new TextOnlyProcessor(chatGPTClient);
 	}
