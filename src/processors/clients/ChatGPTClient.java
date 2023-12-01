b3305a3e913ad7bfea982ac67789b25f5b0bb77a
Nipat Tuntasood <ntuntasood@gmail.com>
2023-12-01 02:34:32 -0800

    Integrate with ChatGPT

--- src/processors/clients/ChatGPTClient.java
+++ src/processors/clients/ChatGPTClient.java
@@ -1,10 +1,42 @@
 package processors.clients;
 
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import common.PropertiesReader;
+import common.clients.HttpClient;
+import common.clients.HttpClient.RequestMethod;
 
 public class ChatGPTClient {
-	
-	public String makeRequest(String input) {
-		return "dummy result";
+
+	private PropertiesReader properties = PropertiesReader.getInstance();
+	private HttpClient httpClient;
+
+	public ChatGPTClient() {
+		httpClient = new HttpClient();
 	}
+
+	public String makeRequest(String prompt) {
+		String url = properties.getProperty("openai.url");
+		String secretkey = properties.getProperty("openai.secretkey");
+		String model = properties.getProperty("openai.model");
+
+		Map<String, String> headers = Map.of(
+				"Authorization", "Bearer " + secretkey,
+				"Content-Type", "application/json"
+				);
+
+		JSONObject body = new JSONObject()
+				.put("model", model)
+				.put("messages", new JSONArray(List.of(new JSONObject()
+						.put("role", "user")
+						.put("content", prompt))));
+
+		return httpClient.makeRequest(RequestMethod.POST, url, headers, body);
+	}
+
 
 }
