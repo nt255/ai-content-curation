7c77cc18df8ad3b0493a9b7e01fece520636363c
nt255 <nt255@users.noreply.github.com>
2023-12-01 21:03:26 -0800

    Add skeleton classes for MessageQueue (#9)
    * Add lombok annotation processor
    
    * Change all tabs to spaces
    
    * Fix pom.xml tabbing
    
    * Add skeleton classes for MessageQueue
    
    * Add skeleton classes for MessageQueue

---
+++ src/server/ServerModule.java
@@ -0,0 +1,13 @@
+package server;
+
+import com.google.inject.AbstractModule;
+
+
+public class ServerModule extends AbstractModule {
+    
+    @Override
+    protected void configure() {
+
+    }
+
+}
