26652df2d63738dc81428ab6303e094d21f1fe0c
Nipat Tuntasood <ntuntasood@gmail.com>
2023-12-11 21:57:19 -0800

    Add more tests

--- src/main/java/common/file/FileServer.java
+++ src/main/java/common/file/FileServer.java
@@ -1,12 +1,13 @@
 package main.java.common.file;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public interface FileServer {
     
     public void uploadFile(String name, String sourcePath);
     
-    public File downloadFile(String name);
+    public File downloadFile(String name) throws FileNotFoundException;
     
     public void deleteFile(String name);
 
